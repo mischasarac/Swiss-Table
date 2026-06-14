f983501aef11e55ea7ee20486bd07fa2f9148d27
mischasarac <mischasarac@gmail.com>
2026-06-14 17:29:26 +0930

    feat(iterator): pivoted return type of swiss map to have iterator return types. Implemented core iterator functions.

--- src/swiss_map.h
+++ src/swiss_map.h
@@ -32,9 +32,9 @@ template <
 >
 requires Hashable<K, Hash>
 class swiss_map {
-private: // Member variables
+    private: // Member variables
     size_t size_{};
-    size_t bucketCount_{};
+    size_t bucket_count_{};
     float growth_factor{2.0};
 
     std::vector<ctrl_t> ctrl_{};
@@ -54,44 +54,11 @@ private: // Struct definitions
     };
 
 
-private: // Internal helper functions
-
-    // Cache-related helpers
-    size_t H1(size_t hash) const;    // Hash in slot of table
-    ctrl_t H2(size_t hash) const; // Hash within the control (bitmask)
-
-    void expand();
-
-    uint16_t match(size_t index, h2_t hash) const;
-    uint16_t match_empty(size_t index) const;
-    uint16_t match_free_slot(size_t index) const;
-
-    void set_table_size(size_t n);
-
-    void delete_at_index(size_t index);
-    
-
 public:
+/*
+==================== Iterators ====================
+*/
 
-    // Default constructors and deconstructors
-    swiss_map();
-    swiss_map(size_t n);
-    swiss_map(const swiss_map&) = default;
-    swiss_map(swiss_map&&) = default;
-    swiss_map(std::initializer_list<std::pair<K, V>> list);
-
-    ~swiss_map() = default;
-
-    // Standard function
-    V& at(const K& key);
-
-    V& insert(const K& key, const V& value);
-
-    V& operator[](const K& key);
-
-    void erase(const K& key);
-
-public:
     struct Iterator {
         using iterator_category = std::forward_iterator_tag;
         using iterator_concept = std::forward_iterator_tag;
@@ -118,8 +85,59 @@ public:
     private:
         swiss_map* map_;
         size_t index_;
+        friend class swiss_map;
 
     };
+
+    Iterator begin();
+    Iterator end();
+
+    Iterator find(const K& key);
+
+
+
+private: // Internal helper functions
+
+    // Cache-related helpers
+    size_t H1(size_t hash) const;    // Hash in slot of table
+    ctrl_t H2(size_t hash) const; // Hash within the control (bitmask)
+
+    void expand();
+
+    bool occupied(size_t index) const;
+
+    uint16_t match(size_t index, h2_t hash) const;
+    uint16_t match_empty(size_t index) const;
+    uint16_t match_free_slot(size_t index) const;
+
+    void set_table_size(size_t n);
+
+    Iterator delete_at_index(size_t index);
+
+    Iterator find_next_iterator(size_t index);
+    Iterator find_next_iterator(Iterator it);
+    
+
+public:
+
+    // Default constructors and deconstructors
+    swiss_map();
+    swiss_map(size_t n);
+    swiss_map(const swiss_map&) = default;
+    swiss_map(swiss_map&&) = default;
+    swiss_map(std::initializer_list<std::pair<K, V>> list);
+
+    ~swiss_map() = default;
+
+    // Standard function
+    V& at(const K& key);
+
+    V& insert(const K& key, const V& value);
+
+    V& operator[](const K& key);
+
+    Iterator erase(const K& key);
+    Iterator erase(Iterator it);
 
 };
 
